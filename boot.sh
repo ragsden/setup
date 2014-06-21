2175a29ff1b73756daa4df9041fa98273f087f8b
ragsden <raghu@shippable.com>
2014-06-21 16:28:59 -0700

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -2,5 +2,5 @@
 echo 'creating shippable account'
 sudo useradd -d /home/shippable -m -s /bin/bash -p shippablepwd shippable && sudo echo 'shippable ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers
 echo 'created shippable user'
-echo 'run the following command "curl -s https://github.com/shippable/install/setup.sh | sudo sh"'
+echo 'run the following command "git clone https://github.com/ragsden/setup.git && cd setup && . setup.sh /n"'
 sudo su - shippable
